95fd451028998f42e010bede941218cc54830399
VladZinko <zinko1996@yandex.ru>
2024-05-20 00:38:49 +0300

    modified CommandFactory
    the use of UserService and USerDao instances components has been changed to beans

--- src/main/java/com/zinko/conf/CommandFactory.java
+++ src/main/java/com/zinko/conf/CommandFactory.java
@@ -6,11 +6,9 @@ import com.zinko.controller.commands.impl.*;
 import com.zinko.data.dao.BookDao;
 import com.zinko.data.dao.UserDao;
 import com.zinko.data.dao.connection.MyConnectionManager;
-import com.zinko.data.dao.impl.BookDaoImpl;
 import com.zinko.data.dao.impl.UserDaoImpl;
 import com.zinko.service.BookService;
 import com.zinko.service.UserService;
-import com.zinko.service.impl.BookServiceImpl;
 import com.zinko.service.impl.UserServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -26,13 +24,7 @@ import java.util.Map;
 public class CommandFactory {
 
     private static final CommandFactory INSTANCE = new CommandFactory();
-    private static final String DB_URL_KEY = "db.url";
-    private static final String DB_PASSWORD_KEY = "db.password";
-    private static final String DB_USER_KEY = "db.user";
-    private static final String DB_DRIVER_KEY = "db.driver";
-    private static final String DB_POOL_SIZE_KEY = "db.poolSize";
     private final List<Closeable> resources;
-
     private final Map<String, Command> commands;
 
     private CommandFactory() {
@@ -45,10 +37,10 @@ public class CommandFactory {
         resources.add(myConnectionManager);
 
         BookDao bookDao = context.getBean(BookDao.class);
-        UserDao userDao = new UserDaoImpl(myConnectionManager);
+        UserDao userDao = context.getBean(UserDao.class);
 
         BookService bookService = context.getBean(BookService.class);
-        UserService userService = new UserServiceImpl(userDao);
+        UserService userService = context.getBean(UserService.class);
 
         commands.put("books", new BooksCommand(bookService));
         commands.put("users", new UsersCommand(userService));
