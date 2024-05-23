4021de9462cd5c9145b28dc003e308562909790f
VladZinko <zinko1996@yandex.ru>
2024-05-23 18:43:31 +0300

    added beans for JdbcTemplate, NamedParameterJdbcTemplate, HikariDataSource
    deleted old classes for connection

--- src/main/java/com/zinko/AppContext.java
+++ src/main/java/com/zinko/AppContext.java
@@ -1,11 +1,53 @@
 package com.zinko;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import javax.sql.DataSource;
+
 
 @Configuration
 @ComponentScan
 @PropertySource("classpath:/application.properties")
 public class AppContext {
+
+    @Value("${db.driver}")
+    private String driver;
+    @Value("${db.url}")
+    private String url;
+    @Value("${db.user}")
+    private String user;
+    @Value("${db.password}")
+    private String password;
+    @Value("${db.poolSize}")
+    private int poolSize;
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        return jdbcTemplate;
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
+        NamedParameterJdbcTemplate namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+        return namedParameterJdbcTemplate;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(url);
+        dataSource.setUsername(user);
+        dataSource.setPassword(password);
+        dataSource.setMaximumPoolSize(poolSize);
+        dataSource.setDriverClassName(driver);
+        return dataSource;
+    }
 }
