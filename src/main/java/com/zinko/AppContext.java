533b9b488eb9961327836d278be40ed8dfe76a51
VladZinko <zinko1996@yandex.ru>
2024-06-11 12:11:48 +0300

    added beans for persistence modified persistence.xml, updated pom.xml

--- src/main/java/com/zinko/AppContext.java
+++ src/main/java/com/zinko/AppContext.java
@@ -1,61 +1,20 @@
 package com.zinko;
 
-import com.zaxxer.hikari.HikariDataSource;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.TransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.sql.DataSource;
 
 
 @Configuration
 @ComponentScan
 @EnableTransactionManagement
-@PropertySource("classpath:/application.properties")
 public class AppContext {
-
-    @Value("${db.driver}")
-    private String driver;
-    @Value("${db.url}")
-    private String url;
-    @Value("${db.user}")
-    private String user;
-    @Value("${db.password}")
-    private String password;
-    @Value("${db.poolSize}")
-    private int poolSize;
-
-    @Bean
-    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-        return jdbcTemplate;
-    }
-
-    @Bean
-    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
-        NamedParameterJdbcTemplate namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
-        return namedParameterJdbcTemplate;
-    }
-
-    @Bean
-    public DataSource dataSource() {
-        HikariDataSource dataSource = new HikariDataSource();
-        dataSource.setJdbcUrl(url);
-        dataSource.setUsername(user);
-        dataSource.setPassword(password);
-        dataSource.setMaximumPoolSize(poolSize);
-        dataSource.setDriverClassName(driver);
-        return dataSource;
-    }
 
     @Bean
     public EntityManagerFactory entityManagerFactory() {
