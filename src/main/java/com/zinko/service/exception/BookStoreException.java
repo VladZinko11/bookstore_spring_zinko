9734591af252436916b7e9604f682ceba98d7429
VladZinko <zinko1996@yandex.ru>
2024-06-21 22:29:22 +0300

    modified exceptions classes, added new exception class NonAccessException that will throws for non access req

---
+++ src/main/java/com/zinko/service/exception/BookStoreException.java
@@ -0,0 +1,9 @@
+package com.zinko.service.exception;
+
+public class BookStoreException extends RuntimeException {
+
+    public BookStoreException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/com/zinko/service/exception/FailedLoginException.java
@@ -0,0 +1,8 @@
+package com.zinko.service.exception;
+
+public class FailedLoginException extends BookStoreException {
+
+    public FailedLoginException(String message) {
+        super(message);
+    }
+}
